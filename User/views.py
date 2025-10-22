9e02d6f7f804c59044a3256c19655dba2236ef95
Sarisha.tk <sarishakrishna58@gmail.com>
2025-10-22 08:34:06 +0530

    today initaial commit

--- User/views.py
+++ User/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
+from User.models import User
 
 from User.froms import UserRegistrationForm
 # Create your views here.
@@ -15,5 +16,25 @@ class RegisterView(View):
         form = UserRegistrationForm()
 
         return render (request,"signin.html",{'form':form})
+    
+    def post(self,request):
+
+        print(request.POST)
+
+        username = request.POST.get("username")
+
+        first_name =  request.POST.get("first_name")
+
+        last_name =  request.POST.get("last_name")
+
+        password = request.POST.get("password")
+
+        email =  request.POST.get("email")
+
+        User.objects.create_user(username =username,first_name = first_name, last_name = last_name,password=password,email=email )
+
+        form = UserRegistrationForm()
+
+        return render(request,"signin.html",{'form':form})
 
 
